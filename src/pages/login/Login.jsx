dddaa6a1ef3770ce50e8b30eb0ef924078f62d61
naomi <naomi@program.com.tw>
2024-12-17 17:26:01 +0800

    feat: [login & register page] page design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./login.scss";
 
 export const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            necessitatibus.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username"></input>
+            <input type="password" placeholder="Password"></input>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-import "./register.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./register.scss";
 
 export const Register = () => {
   return (
-    <div className='register'>Register</div>
-  )
-}
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>CXR School</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            necessitatibus.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username"></input>
+            <input type="email" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <input type="text" placeholder="Name"></input>
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
