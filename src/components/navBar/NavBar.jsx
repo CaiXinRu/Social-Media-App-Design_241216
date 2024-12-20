517bf54ac9d50dae3d6ebd93fa8b5961cdd60ce9
naomi <naomi@program.com.tw>
2024-12-20 11:19:43 +0800

    feat: design navbar initially

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,40 @@
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import React from "react";
+import { Link } from "react-router-dom";
+import "./navBar.scss";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>RUSocial</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://picsum.photos/30" />
+          <span>CXR</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
