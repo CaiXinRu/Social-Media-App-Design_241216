4a103cff9d64d70689f4c88a055697a505c53c34
naomi <naomi@program.com.tw>
2024-12-20 11:13:55 +0800

    fix: sass warning issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        sassOptions: {
+          api: "modern", // or "modern-compiler".
+        },
+        silenceDeprecations: ["legacy-js-api"],
+      },
+    },
+  },
+});
