4e913ffe7ba5dfd50c401e2fecb5804ebd279114
naomi <naomi@program.com.tw>
2024-12-25 13:52:08 +0800

    feat: profile UI design initially

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,65 @@
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LanguageIcon from "@mui/icons-material/Language";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import PlaceIcon from "@mui/icons-material/Place";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import React from "react";
+import Posts from "../../components/posts/Posts";
 import "./profile.scss";
 
 const Profile = () => {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img src="https://picsum.photos/700" alt="" className="cover" />
+        <img src="https://picsum.photos/550" alt="" className="profilePic" />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://instagram.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Carol</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Carol.com</span>
+              </div>
+              <button>Follow</button>
+            </div>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
