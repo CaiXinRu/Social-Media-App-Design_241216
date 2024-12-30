4d2f421cbec5595e40df30aace6c276421615dfd
naomi <naomi@program.com.tw>
2024-12-30 16:41:03 +0800

    feat: connect to post/posts api

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import LeftBar from "../components/leftBar/LeftBar";
@@ -8,17 +9,22 @@ import "../style.scss";
 
 const Layout = () => {
   const { darkMode } = useContext(DarkModeContext);
+
+  const queryClient = new QueryClient();
+
   return (
-    <div className={`theme-${darkMode ? "dark" : "light"}`}>
-      <NavBar />
-      <div style={{ display: "flex" }}>
-        <LeftBar />
-        <div style={{ flex: 6 }}>
-          <Outlet />
+    <QueryClientProvider client={queryClient}>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
         </div>
-        <RightBar />
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,32 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { makeRequest } from "../../axios";
 import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-  const posts = [
-    {
-      id: 1,
-      name: "Eric",
-      userId: 10,
-      profilePic: "https://picsum.photos/450",
-      desc: "Et deserunt eu tempor eiusmod sunt voluptate. Ullamco ea deserunt consequat deserunt incididunt aute et velit occaecat minim qui magna anim laboris. Laboris culpa officia labore reprehenderit reprehenderit nulla Lorem magna laborum do tempor ex ex id.",
-      img: "https://picsum.photos/600",
-    },
-    {
-      id: 2,
-      name: "Jessi",
-      userId: 11,
-      profilePic: "https://picsum.photos/450",
-      desc: "Voluptate culpa duis mollit reprehenderit mollit cillum officia magna irure ut. Proident eu ipsum aute aliquip Lorem. Exercitation occaecat incididunt minim irure officia adipisicing sit. Veniam velit deserunt excepteur cillum enim nostrud duis irure in velit pariatur ex. Aliquip consectetur incididunt labore laboris excepteur officia fugiat ipsum. Ad do consequat voluptate ad do non.",
-      img: "https://picsum.photos/600",
-    },
-  ];
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => makeRequest.get("/posts").then((res) => res.data),
+  });
+
+  console.log(data);
 
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "Something went wrong!"
+        : isLoading
+          ? "loading"
+          : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 };
