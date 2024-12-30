e08106b3894ac61e1990df9450bf36281526fc0b
naomi <naomi@program.com.tw>
2024-12-30 16:55:52 +0800

    feat: Add "Share" component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./home.scss";
-import Stories from "../../components/stories/stories";
 import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
+import Stories from "../../components/stories/stories";
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Stories />
+      <Share />
       <Posts />
     </div>
   );
